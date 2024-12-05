20b4138bff1adf43e78e518e2026f261089c6b46
vemm8ks2 <vemm8ks2@gmail.com>
2024-12-05 17:03:12 +0900

    CHORE: 프로젝트 생성

---
+++ src/test/java/com/vemm8ks2/FinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vemm8ks2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
