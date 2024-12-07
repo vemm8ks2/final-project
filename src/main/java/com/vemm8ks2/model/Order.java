7969d12ae3ff054c7a58c1e3316d92b538501b59
vemm8ks2 <vemm8ks2@gmail.com>
2024-12-07 23:07:14 +0900

    FIX: order 테이블 예약어 오류 수정

--- src/main/java/com/vemm8ks2/model/Order.java
+++ src/main/java/com/vemm8ks2/model/Order.java
@@ -13,6 +13,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -23,6 +24,7 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "`order`")
 public class Order {
 
   @Id
