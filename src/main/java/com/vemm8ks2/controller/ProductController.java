03ddded2c436db7f647a207c5f2f2964399d799d
JooseobKim <outfox100@naver.com>
2024-12-09 07:23:09 +0900

    FEAT, REFACTOR: Product controller 추가, JPA 메소드 변경

---
+++ src/main/java/com/vemm8ks2/controller/ProductController.java
@@ -0,0 +1,51 @@
+package com.vemm8ks2.controller;
+
+import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import com.vemm8ks2.exception.ProductException;
+import com.vemm8ks2.model.Product;
+import com.vemm8ks2.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@RequestMapping("/api/products")
+@RequiredArgsConstructor
+@Slf4j
+public class ProductController {
+
+  private final ProductService productService;
+
+  @GetMapping("/")
+  public ResponseEntity<Page<Product>> findProductByCategoryHandler(@RequestParam String category,
+      @RequestParam List<String> colors, @RequestParam List<String> sizes,
+      @RequestParam Integer minPrice, @RequestParam Integer maxPrice,
+      @RequestParam Integer minDiscount, @RequestParam String sort, @RequestParam String stock,
+      @RequestParam Integer pageNumber, @RequestParam Integer pageSize) {
+
+    Page<Product> result = productService.getAllProduct(category, colors, sizes, minPrice, maxPrice,
+        minDiscount, sort, stock, pageNumber, pageSize);
+
+    log.info("|| --- findProductByCategoryHandler");
+
+    return new ResponseEntity<>(result, HttpStatus.ACCEPTED);
+  }
+
+  @GetMapping("/id/{productId}")
+  public ResponseEntity<Product> findProductByIdHandler(@PathVariable Long productId)
+      throws ProductException {
+
+    Product product = productService.findProductById(productId);
+
+    log.info("|| --- findProductByIdHandler");
+
+    return new ResponseEntity<Product>(product, HttpStatus.ACCEPTED);
+  }
+}
--- src/main/java/com/vemm8ks2/repository/CartRepository.java
+++ src/main/java/com/vemm8ks2/repository/CartRepository.java
@@ -1,12 +1,9 @@
 package com.vemm8ks2.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import com.vemm8ks2.model.Cart;
 
 public interface CartRepository extends JpaRepository<Cart, Long> {
 
-  @Query("SELECT c FROM Cart c WHERE c.user.id = :userId")
-  public Cart findByUserId(@Param("userId") Long userId);
+  public Cart findByUserId(Long userId);
 }
